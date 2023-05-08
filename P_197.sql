adc02804dc362c405e9223c714c8105ea774e55c
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-05-08 10:15:12 +0530

    sql leetcode

---
+++ P_197.sql
@@ -0,0 +1,9 @@
+select w1.Id
+from Weather as w1, Weather as w2
+where datediff(w1.RecordDate, w2.RecordDate) = 1 and w1.Temperature >
+w2.Temperature;
+
+-- or
+
+select A.Id from Weather as A, Weather as B 
+where A.Temperature > B.Temperature and TO_DAYS(A.recordDate) = TO_DAYS(B.recordDate) + 1
