16dd05b61cd56c64edbd69ddc16aa0e7d1f68185
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-07-09 18:07:33 +0530

    number of employees which report to each employee

---
+++ P_1731.sql
@@ -0,0 +1,11 @@
+with cte as 
+(select reports_to , count(employee_id) as reports_count , round(avg(age),0) as average_age
+from Employees
+where reports_to is not null
+group by reports_to)
+
+select c.reports_to as employee_id ,e.name,c.reports_count,c.average_age
+from cte c
+left join employees e
+on c.reports_to=e.employee_id
+order by employee_id
