77ab1727d6532042f440e8d66cd687a1d7e14e7c
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-07-09 18:45:09 +0530

    primary department for each employee

---
+++ P_1789.sql
@@ -0,0 +1,4 @@
+select employee_id, case when count(department_id) = 1 then department_id
+when count(department_id)>1 then sum((primary_flag = 'Y')*department_id) end as department_id
+from Employee
+group by employee_id
