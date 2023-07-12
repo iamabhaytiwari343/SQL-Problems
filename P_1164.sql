b2aed6c76073e2ab651271c13c069ed7a375cfcb
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-07-12 20:30:11 +0530

    product price at a given date

---
+++ P_1164.sql
@@ -0,0 +1,10 @@
+with cte as
+(select *,rank()over(PARTITION BY product_id order by change_date desc) as r
+from products
+where change_date<='2019-08-16')
+
+select product_id,new_price as price from cte
+where r=1
+union
+select product_id,10 as price
+from Products where product_id not in (select product_id from cte)
